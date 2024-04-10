1a598368b272d1b07fd17bba1b287af22a2b913b
shivam_uppal <shivsen1997@gmail.com>
2024-04-11 02:12:46 +0530

    commiting ipynb along with the data

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-requirement.txt
+from setuptools import setup, find_packages
+
+setup(
+    name="MCQ_generator", 
+    version=0.01,
+    author="shivam",
+    install_requires=["OpenAi", "langchain", "streamlit", "pyPDF2", "python-dotenv"],
+    packages=find_packages()
+)
